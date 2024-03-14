2c3cfe42520cd32cd9c88c7253e0f8179e53d8c2
daiki510 <ddgg7755@gmail.com>
2024-03-14 23:17:27 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import { client, fetchAllTasks, fetchTaskBy, initDB } from "./pgClient";
+
+await client.connect();
+
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
+  async fetch(req) {
+    await initDB();
+    const timezone = await client.query("SHOW TIME ZONE");
+    console.log(timezone);
+    const tasks = await fetchAllTasks();
+    const task = await fetchTaskBy(1);
+    return new Response(
+      `task: ${JSON.stringify(task)}\ntasks: ${JSON.stringify(tasks)}`
+    );
   },
 });
 
---
+++ src/pgClient.ts
@@ -0,0 +1,83 @@
+import { Client } from "pg";
+import { load } from "ts-dotenv";
+
+const env = load({
+  // DATABASE_URL: String,
+  PGHOST: String,
+  PGDATABASE: String,
+  PGUSER: String,
+  PGPASSWORD: String,
+  ENDPOINT_ID: String,
+});
+
+// const client = new Client({
+//   connectionString: env.DATABASE_URL,
+// });
+export const client = new Client({
+  user: env.PGUSER,
+  password: env.PGPASSWORD,
+  host: env.PGHOST,
+  database: env.PGDATABASE,
+  port: 5432,
+  ssl: true,
+});
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  createdAt: Date;
+};
+
+export async function initDB() {
+  await dropTasksTable();
+  await createTasksTable();
+  await insertTasks();
+}
+
+async function createTasksTable() {
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS tasks 
+      ( 
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255),
+        is_completed BOOLEAN,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Tokyo')
+      )
+    `
+  );
+}
+
+async function dropTasksTable() {
+  await client.query("DROP TABLE IF EXISTS tasks");
+}
+
+// async function truncateTasksTable() {
+//   await client.query("TRUNCATE tasks");
+// }
+
+async function insertTasks() {
+  const insertValues = [...Array(5).keys()]
+    .map((i) => {
+      const id = i + 1;
+      const title = `'task_${id}'`;
+      const isCompleted = id % 2 === 0;
+      return `(${id}, ${title}, ${isCompleted})`;
+    })
+    .join(", ");
+  await client.query<Task>(
+    `INSERT INTO tasks (id, title, is_completed) VALUES ${insertValues}`
+  );
+}
+
+export async function fetchAllTasks(): Promise<Task[]> {
+  const tasks = await client.query<Task>("SELECT * FROM tasks");
+  return tasks.rows;
+}
+
+export async function fetchTaskBy(id: number): Promise<Task> {
+  const tasks = await client.query<Task>(
+    `SELECT * FROM tasks WHERE id = ${id}`
+  );
+  return tasks.rows[0];
+}
